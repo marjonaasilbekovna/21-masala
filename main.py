3685576226ce30e83493092f2ad97014f74c0617
marjonaasilbekovna <abdurahmonovamarjona007@gmail.com>
2024-12-10 13:15:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 n,k=map(int,input().split())
-print(k%n)a, b, c = map(int, input().split())
+print(k%n)
+a, b, c = map(int, input().split())
 A = (a + 1) // 2 
 B = (b + 1) // 2  
 C = (c + 1) // 2  
